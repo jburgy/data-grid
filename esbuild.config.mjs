d3108770d6c3c18b151909949379ef0140839fac
Jan Burgy <jburgy@gmail.com>
2025-10-03 23:47:17 -0400

    Clear cell outputs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import * as esbuild from 'esbuild'
 import metaUrlPlugin from '@chialab/esbuild-plugin-meta-url';
-// import workerPlugin from '@chialab/esbuild-plugin-worker';
+import workerPlugin from '@chialab/esbuild-plugin-worker';
 
 await esbuild.build({
     entryPoints: ['js/widget.js'],
@@ -8,5 +8,5 @@ await esbuild.build({
     format: 'esm',
     bundle: true,
     outdir: 'src/data_grid/static',
-    plugins: [metaUrlPlugin()],
+    plugins: [metaUrlPlugin(), workerPlugin()],
 })
